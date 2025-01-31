eca43823ff72fc3b21d213c71f3850ebdd061209
Kris <moyazuk@gmail.com>
2025-01-30 19:06:29 -0500

    fix

--- PerformanceCalculatorGUI/Components/StrainVisualizer.cs
+++ PerformanceCalculatorGUI/Components/StrainVisualizer.cs
@@ -17,6 +17,7 @@ using osu.Game.Graphics.Sprites;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Overlays;
 using osu.Game.Rulesets.Difficulty.Skills;
+using osu.Game.Rulesets.Osu.Difficulty.Aggregation;
 using osu.Game.Screens.Edit.Compose.Components.Timeline;
 using osuTK;
 using PerformanceCalculatorGUI.Components.TextBoxes;
@@ -51,7 +52,7 @@ namespace PerformanceCalculatorGUI.Components
         {
             graphsContainer.Clear();
 
-            var skills = val.NewValue.Where(x => x is StrainSkill or StrainDecaySkill).ToArray();
+            var skills = val.NewValue.Where(x => x is StrainSkill or StrainDecaySkill or OsuProbabilitySkill).ToArray();
 
             // dont bother if there are no strain skills to draw
             if (skills.Length == 0)
@@ -62,9 +63,21 @@ namespace PerformanceCalculatorGUI.Components
             }
 
             graphAlpha = Math.Min(1.5f / skills.Length, 0.9f);
-            var strainLists = getStrainLists(skills);
-            addStrainBars(skills, strainLists);
-            addTooltipBars(strainLists);
+            var (strainLists, timestamps) = getStrainLists(skills);
+            for (int i = 0; i < skills.Length; i++)
+            {
+                if (timestamps != null && i < timestamps.Count && timestamps[i] != null)
+                {
+                    //  if we're placing individual note timestamps
+                    addNoteBarsWithTimestamps(i, strainLists[i], timestamps[i]);
+                }
+                else
+                {
+                    // if we don't have timestamps, don't use them.
+                    addNoteBarsWithoutTimestamps(i, strainLists[i]);
+                }
+            }
+            addTooltipBars(strainLists, timestamps);
 
             if (val.OldValue == null || !val.NewValue.All(x => val.OldValue.Any(y => y.GetType().Name == x.GetType().Name)))
             {
@@ -184,28 +197,47 @@ namespace PerformanceCalculatorGUI.Components
             Skills.BindValueChanged(updateGraphs);
         }
 
-        private void addStrainBars(Skill[] skills, List<float[]> strainLists)
+        private void addNoteBarsWithTimestamps(int index, float[] strainLists, float[] timestamps)
         {
-            var strainMaxValue = strainLists.Max(list => list.Max());
+            var strainMaxValue = strainLists.Max();
 
-            for (int i = 0; i < skills.Length; i++)
+            graphsContainer.AddRange(new Drawable[]
             {
-                graphsContainer.AddRange(new Drawable[]
+                new BufferedContainer(cachedFrameBuffer: true)
                 {
-                    new BufferedContainer(cachedFrameBuffer: true)
+                    RelativeSizeAxes = Axes.Both,
+                    Alpha = graphAlpha,
+                    Colour = skillColours[index],
+                    Child = new StrainBarGraph
                     {
                         RelativeSizeAxes = Axes.Both,
-                        Alpha = graphAlpha,
-                        Colour = skillColours[i],
-                        Child = new StrainBarGraph
-                        {
-                            RelativeSizeAxes = Axes.Both,
-                            MaxValue = strainMaxValue,
-                            Values = strainLists[i]
-                        }
+                        MaxValue = strainMaxValue,
+                        Values = strainLists,
+                        Timestamps = timestamps
                     }
-                });
-            }
+                }
+            });
+        }
+
+        private void addNoteBarsWithoutTimestamps(int index, float[] strainLists)
+        {
+            var strainMaxValue = strainLists.Max();
+
+            graphsContainer.AddRange(new Drawable[]
+            {
+                new BufferedContainer(cachedFrameBuffer: true)
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Alpha = graphAlpha,
+                    Colour = skillColours[index],
+                    Child = new StrainBarGraph
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        MaxValue = strainMaxValue,
+                        Values = strainLists,
+                    }
+                }
+            });
 
             graphsContainer.Add(new OsuSpriteText
             {
@@ -214,54 +246,65 @@ namespace PerformanceCalculatorGUI.Components
             });
         }
 
-        private void addTooltipBars(List<float[]> strainLists, int nBars = 1000)
+    private void addTooltipBars(List<float[]> strainLists, List<float[]>? timestamps = null, int nBars = 1000)
+    {
+        double lastStrainTime;
+
+        if (timestamps != null && timestamps.Any())
         {
-            double lastStrainTime = strainLists.Max(l => l.Length) * 400;
 
-            var tooltipList = new List<string>();
+            lastStrainTime = timestamps.Max(t => t.LastOrDefault());
+        }
+        else
+        {
 
-            for (int i = 0; i < nBars; i++)
-            {
-                var strainTime = TimeSpan.FromMilliseconds(TimeUntilFirstStrain.Value + lastStrainTime * i / nBars);
-                var tooltipText = $"~{strainTime:mm\\:ss\\.ff}";
-                tooltipList.Add(tooltipText);
-            }
-
-            graphsContainer.AddRange(new Drawable[]
-            {
-                new BufferedContainer(cachedFrameBuffer: true)
-                {
-                    RelativeSizeAxes = Axes.Both,
-                    Alpha = 1,
-                    Child = new TooltipBarGraph
-                    {
-                        RelativeSizeAxes = Axes.Both,
-                        Values = tooltipList
-                    }
-                }
-            });
+            lastStrainTime = strainLists.Max(l => l.Length) * 400;
         }
 
-        private static List<float[]> getStrainLists(Skill[] skills)
-        {
-            List<float[]> strainLists = new List<float[]>();
+        var tooltipList = new List<string>();
 
+        for (int i = 0; i < nBars; i++)
+        {
+
+            var strainTime = TimeSpan.FromMilliseconds(TimeUntilFirstStrain.Value + lastStrainTime * i / nBars);
+            var tooltipText = $"~{strainTime:mm\\:ss\\.ff}";
+            tooltipList.Add(tooltipText);
+        }
+
+        graphsContainer.AddRange(new Drawable[]
+        {
+            new BufferedContainer(cachedFrameBuffer: true)
+            {
+                RelativeSizeAxes = Axes.Both,
+                Alpha = 1,
+                Child = new TooltipBarGraph
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Values = tooltipList
+                }
+            }
+        });
+    }
+
+        private static (List<float[]> StrainLists, List<float[]>? Timestamps) getStrainLists(Skill[] skills)
+        {
+            var strainLists = new List<float[]>();
+            var timestamps = new List<float[]>();
             foreach (var skill in skills)
             {
-                var strains = ((StrainSkill)skill).GetCurrentStrainPeaks().ToArray();
-
-                var skillStrainList = new List<float>();
-
-                for (int i = 0; i < strains.Length; i++)
+                if (skill is OsuProbabilitySkill probSkill)
                 {
-                    var strain = strains[i];
-                    skillStrainList.Add(((float)strain));
+                    var data = probSkill.GetCurrentStrainPeaks();
+                    strainLists.Add(data.Select(d => (float)d.Value).ToArray());
+                    timestamps.Add(data.Select(d => (float)d.Timestamp).ToArray());
                 }
-
-                strainLists.Add(skillStrainList.ToArray());
+                else if (skill is StrainSkill strainSkill)
+                {
+                    strainLists.Add(strainSkill.GetCurrentStrainPeaks().Select(d => (float)d).ToArray());
+                }
             }
 
-            return strainLists;
+            return (strainLists, timestamps.Count > 0 ? timestamps : null);
         }
     }
 
@@ -275,28 +318,108 @@ namespace PerformanceCalculatorGUI.Components
         /// <summary>
         /// A list of floats that defines the length of each <see cref="Bar"/>
         /// </summary>
-        public IEnumerable<float> Values
+        public IEnumerable<float> Values { get; set; }
+
+        /// <summary>
+        /// Nullable timestamps for per note skills
+        /// </summary>
+        public IEnumerable<float>? Timestamps { get; set; }
+
+        protected override void LoadComplete()
         {
-            set
+            base.LoadComplete();
+            UpdateBars();
+        }
+
+        private void UpdateBars()
+        {
+            Clear();
+
+            if (Values == null || !Values.Any())
+                return;
+
+            float[] valuesArray = Values.ToArray();
+            float maxValue = MaxValue ?? valuesArray.Max();
+
+            if (Timestamps != null)
             {
-                Clear();
-
-                foreach (var val in value)
+                // for per note skills
+                float[] localTimestampsArray = Timestamps.ToArray();
+                if (localTimestampsArray.Length == 0 || localTimestampsArray.Last() == 0)
                 {
-                    float length = MaxValue ?? value.Max();
-                    if (length != 0)
-                        length = val / length;
+                    throw new ArgumentException("Timestamps are empty or have an invalid range.");
+                }
 
-                    float size = value.Count();
-                    if (size != 0)
-                        size = 1.0f / size;
+                float totalDuration = localTimestampsArray.Last();
+                float shortestInterval = float.MaxValue;
 
+                // probably not the best way to do this, but tries to get the best bar width by calculating the shortest straintime in the map
+                for (int i = 1; i < localTimestampsArray.Length; i++)
+                {
+                    float interval = localTimestampsArray[i] - localTimestampsArray[i - 1];
+                    if (interval > 0) shortestInterval = Math.Min(shortestInterval, interval);
+                }
+
+                if (shortestInterval <= 0)
+                    throw new ArgumentException("Invalid timestamp intervals.");
+
+                int totalBars = (int)Math.Ceiling(totalDuration / shortestInterval);
+                float barWidth = 1.0f / totalBars;
+
+                int currentTimestampIndex = 0;
+                int totalBarsLogged = 0;
+
+
+                for (int i = 0; i < totalBars; i++)
+                {
+                    float currentTime = i * shortestInterval;
+
+                    // matching current position to actual note location
+                    if (currentTimestampIndex < localTimestampsArray.Length &&
+                        Math.Abs(currentTime - localTimestampsArray[currentTimestampIndex]) < shortestInterval / 2)
+                    {
+                        // put a bar
+                        Add(new Bar
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Size = new Vector2(barWidth, 1),
+                            Length = valuesArray[currentTimestampIndex] / maxValue,
+                            Direction = BarDirection.BottomToTop,
+                        });
+
+                        // Console.WriteLine($"[BAR {totalBarsLogged + 1}] Note: Timestamp={TimeSpan.FromMilliseconds(localTimestampsArray[currentTimestampIndex]):mm\\:ss}, Value={valuesArray[currentTimestampIndex]}, Value={valuesArray[currentTimestampIndex]}, Index={currentTimestampIndex + 1}/{localTimestampsArray.Length}");
+
+                        currentTimestampIndex++;
+                    }
+                    else
+                    {
+                        // skip this timestamp if there's note with an empty bar
+                        Add(new Bar
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Size = new Vector2(barWidth, 1),
+                            Length = 0,
+                            Direction = BarDirection.BottomToTop,
+                        });
+
+                        // Console.WriteLine($"[BAR {totalBarsLogged + 1}] Empty: Timestamp={TimeSpan.FromMilliseconds(localTimestampsArray[currentTimestampIndex]):mm\\:ss}, Value={valuesArray[currentTimestampIndex]} (not aligned to note)");
+                    }
+                    totalBarsLogged++;
+                }
+            }
+            else
+            {
+                // StrainSkill: Uniformly space bars
+                float barWidth = 1.0f / valuesArray.Length;
+
+                for (int i = 0; i < valuesArray.Length; i++)
+                {
                     Add(new Bar
                     {
                         RelativeSizeAxes = Axes.Both,
-                        Size = new Vector2(size, 1),
-                        Length = length,
-                        Direction = BarDirection.BottomToTop
+                        Size = new Vector2(barWidth, 1),
+                        Length = valuesArray[i] / maxValue,
+                        Direction = BarDirection.BottomToTop,
                     });
                 }
             }
